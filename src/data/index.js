da70506887003f51236fa75ab82db618533b486c
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-09-27 01:07:32 -0400

    fix import

--- src/data/index.js
+++ src/data/index.js
@@ -1,7 +1,6 @@
 import { dictGroupByKey } from "./dictGroupByKey";
 import { dictByKey } from "./dictByKey";
 import { filterFalse } from "./filterFalse";
-import { filterEmpty } from "./filterEmpty";
 import { uniqValuesByKey } from "./uniqValuesByKey";
 
 export { dictGroupByKey, dictByKey, filterFalse, filterEmpty, uniqValuesByKey };
