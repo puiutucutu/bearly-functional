eaedab85a8dea2056672d675b63c09bdc070ff7d
puiu <5474275+puiutucutu@users.noreply.github.com>
2019-10-27 18:06:55 -0400

    Added `isString` fn

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 import { getPrototype } from "./getPrototype";
 import { isArray } from "./isArray";
+import { isString } from "./isString";
 import { toSignificantFigures } from "./toSignificantFigures";
 
-export { getPrototype, isArray, toSignificantFigures };
+export { getPrototype, isArray, isString, toSignificantFigures };
