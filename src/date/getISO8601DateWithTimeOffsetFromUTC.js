36665ae2ce6e7c904045298530dbfe441bc67164
puiu <5474275+puiutucutu@users.noreply.github.com>
2020-01-23 23:26:25 -0500

    update fn to allow microseconds

--- src/date/getISO8601DateWithTimeOffsetFromUTC.js
+++ src/date/getISO8601DateWithTimeOffsetFromUTC.js
@@ -2,9 +2,16 @@ import { format } from "date-fns";
 
 /**
  * @return {string}
- * @example getISO8601DateWithTimeOffsetFromUTC(); //=> "2007-04-05T12:30-02:00"
  * @see https://en.wikipedia.org/wiki/ISO_8601#Time_zone_designators
+ *
+ * @example
+ *
+ * getISO8601DateWithTimeOffsetFromUTC(); //=> "2007-04-05T12:30-02:00"
+ * getISO8601DateWithTimeOffsetFromUTC(true); //=> "2020-01-23T14:33:15.4260000-05:00"
+ *
  */
-export const getISO8601DateWithTimeOffsetFromUTC = () => {
-  return format(new Date(), "yyyy-MM-dd'T'HH:mm:ssxxx");
+export const getISO8601DateWithTimeOffsetFromUTC = (microseconds = false) => {
+  return microseconds
+    ? format(new Date(), "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSxxx")
+    : format(new Date(), "yyyy-MM-dd'T'HH:mm:ssxxx");
 };
