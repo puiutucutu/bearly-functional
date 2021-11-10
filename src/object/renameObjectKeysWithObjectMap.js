50c4843e40bb5f11a6b91174b745d9536dee8ec0
puiu <>
2021-11-10 11:31:39 -0500

    add

---
+++ src/object/renameObjectKeysWithObjectMap.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Object} defMap
+ * @return {function(obj: Object): Object}
+ * @example
+ * const defMap = { CAN: "Canada", USA: "United States of America" };
+ * const defined = renameObjectKeysWithObjectMap (defMap) ({ CAN: 1867, USA: 1776 }); //=> { "Canada": 1867, "United States of America": 1776 }
+ */
+export const renameObjectKeysWithObjectMap = (defMap) => (obj) => {
+  const next = {};
+  for (const [k, v] of Object.entries(defMap)) {
+    next[v] = obj[k];
+  }
+  return next;
+};
+
+export const alternate = (defMap) => (obj) => {
+  const next = {};
+  Object.entries(defMap).forEach(([k, v]) => (next[v] = obj[k]));
+};
+
