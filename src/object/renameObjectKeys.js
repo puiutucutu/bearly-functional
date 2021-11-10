482a22170a772691de4b7da51336da8a9575c6b6
puiu <>
2021-11-10 11:51:55 -0500

    change fn nature

--- src/object/renameObjectKeys.js
+++ src/object/renameObjectKeys.js
@@ -1,25 +1,33 @@
-import { reduce } from "../list";
+import { PROTOTYPES, prototype } from "../utils";
+import { renameObjectKeysWithMap } from "./renameObjectKeysWithMap";
+import { renameObjectKeysWithObjectMap } from "./renameObjectKeysWithObjectMap";
 
 /**
- * @param {{k: string, v: string}} defMap
+ * @param {Object|Map} defMap
  * @return {function(obj: Object): Object}
+ *
  * @example
  *
  * const defMap = { CAN: "Canada", USA: "United States of America" };
  * const defined = renameObjectKeys (defMap) ({ CAN: 1867, USA: 1776 }); //=> { "Canada": 1867, "United States of America": 1776 }
+ *
+ * @example
+ *
+ * const defMap = new Map();
+ * defMap.set("CAN", "Canada");
+ * defMap.set("USA", "United States of America");
+ *
+ * const defined = renameObjectKeysWithMap (defMap) ({ CAN: 1867, USA: 1776 }); //=> { "Canada": 1867, "United States of America": 1776 }
+ *
  */
 export const renameObjectKeys = (defMap) => (obj) => {
-  const next = {};
-  for (const [k, v] of Object.entries(defMap)) {
-    next[v] = obj[k];
+  if (prototype(defMap) === PROTOTYPES.MAP) {
+    return renameObjectKeysWithObjectMap(defMap)(obj);
   }
-  Object.entries(defMap).forEach(([k, v]) => (next[v] = obj[k]));
-  return next;
-};
 
-const alternate = (defMap) => (obj) =>
-  reduce
-    ((acc) => ([key, value]) => (acc[value] = obj[key], acc))
-    ({})
-    (Object.entries(defMap))
-  ;
+  if (prototype(defMap) === PROTOTYPES.OBJECT) {
+    return renameObjectKeysWithMap(defMap)(obj);
+  }
+
+  throw new Error("`defMap` type must be one of `Object` or `Map`");
+};
