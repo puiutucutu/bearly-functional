59643747c418e023ae488dbfe8254ab4c9aa5076
puiu <>
2021-11-10 11:35:21 -0500

    update ex

--- src/object/renameObjectKeysWithMap.js
+++ src/object/renameObjectKeysWithMap.js
@@ -7,7 +7,7 @@
  * defMap.set("CAN", "Canada");
  * defMap.set("USA", "United States of America");
  *
- * const defined = renameObjectKeys (defMap) ({ CAN: 1867, USA: 1776 }); //=> { "Canada": 1867, "United States of America": 1776 }
+ * const defined = renameObjectKeysWithMap (defMap) ({ CAN: 1867, USA: 1776 }); //=> { "Canada": 1867, "United States of America": 1776 }
  */
 export const renameObjectKeysWithMap = (defMap) => (obj) => {
   const next = {};
